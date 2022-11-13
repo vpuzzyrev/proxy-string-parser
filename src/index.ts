153a254aee307108eb98c852c608c68453759aef
Vladislav <vladislav.puzzyrev@gmail.com>
2022-11-13 20:19:32 +0500

    feat: AxiosProxyConfig export

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,4 @@
-import { AxiosProxyConfig } from 'axios'
+import proxyParser from './proxyParser'
 
-const proxyParser = (proxy: string): AxiosProxyConfig => {
-  const protocolSplit = proxy.split('://')
-  const protocol = protocolSplit.length === 1 ? null : protocolSplit[0]
-  const rest = protocolSplit.length === 1 ? protocolSplit[0] : protocolSplit[1]
-
-  const authSplit = rest.split('@')
-
-  if (authSplit.length === 1) {
-    const host = authSplit[0].split(':')[0]
-    const port = Number(authSplit[0].split(':')[1])
-
-    const proxyConfig: AxiosProxyConfig = {
-      host,
-      port
-    }
-
-    if (protocol != null) {
-      proxyConfig.protocol = protocol
-    }
-
-    return proxyConfig
-  }
-
-  const host = authSplit[1].split(':')[0]
-  const port = Number(authSplit[1].split(':')[1])
-  const [username, password] = authSplit[0].split(':')
-
-  const proxyConfig: AxiosProxyConfig = {
-    host,
-    port,
-    auth: {
-      username,
-      password
-    }
-  }
-
-  if (protocol != null) {
-    proxyConfig.protocol = protocol
-  }
-
-  return proxyConfig
-}
-
+export { AxiosProxyConfig } from 'axios'
 export default proxyParser

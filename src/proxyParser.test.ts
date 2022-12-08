e0e32c652c093265cac492843e2a5704b3e847fd
Vladislav Puzyrev <vladislav.puzzyrev@gmail.com>
2022-12-09 02:28:18 +0500

    test: added tests

---
+++ src/proxyParser.test.ts
@@ -0,0 +1,53 @@
+import proxyParser from './proxyParser.js'
+
+describe('proxyParser', () => {
+  test('full string', () => {
+    const string = 'protocol://username:password@host:80'
+    const proxy = proxyParser(string)
+
+    expect(proxy).toStrictEqual({
+      host: 'host',
+      port: 80,
+      auth: {
+        username: 'username',
+        password: 'password'
+      },
+      protocol: 'protocol'
+    })
+  })
+
+  test('without auth', () => {
+    const string = 'protocol://host:80'
+    const proxy = proxyParser(string)
+
+    expect(proxy).toStrictEqual({
+      host: 'host',
+      port: 80,
+      protocol: 'protocol'
+    })
+  })
+
+  test('without protocol', () => {
+    const string = 'username:password@host:80'
+    const proxy = proxyParser(string)
+
+    expect(proxy).toStrictEqual({
+      host: 'host',
+      port: 80,
+      auth: {
+        username: 'username',
+        password: 'password'
+      }
+    })
+  })
+
+  test('minimal string', () => {
+    const string = 'host:80'
+    const proxy = proxyParser(string)
+
+    expect(proxy).toStrictEqual({
+      host: 'host',
+      port: 80
+    })
+  })
+})
